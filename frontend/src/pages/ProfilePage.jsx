f03b59532dbd89332adda9ea1d5b8dfaa32d6266
Sunny <155150793+sandeep-310@users.noreply.github.com>
2025-09-27 14:19:02 +0530

    created profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,9 +1,75 @@
-const ProfilePage = () => {
-  return (
-    <div>
-      Profile page
-    </div>
-  )
-}
+import { useState } from "react";
+import assets from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-export default ProfilePage
+const ProfilePage = () => {
+  const navigate = useNavigate();
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [name, setName] = useState("sunny");
+  const [bio, setBio] = useState("second year cs student");
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    navigate("/");
+  }
+
+  return (
+    <div className="min-h-screen bg-cover bg-no-repeat flex  items-center justify-center">
+      <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg">
+        <form onSubmit={submitHandler} className="flex flex-col gap-5 p-10 flex-1">
+          <h3 className="text-lg">Profile Details</h3>
+          <label
+            htmlFor="avatar"
+            className="flex items-center gap-3 cursor-pointer"
+          >
+            <input
+              onChange={(e) => setSelectedImage(e.target.files[0])}
+              type="file"
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={
+                selectedImage
+                  ? URL.createObjectURL(selectedImage)
+                  : assets.avatar_icon
+              }
+              alt=""
+              className={`w-12 h-12 ${selectedImage && "rounded-full"}`}
+            />
+            upload profile image
+          </label>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            required
+            placeholder="Your Name"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            required
+            placeholder="Write profile bio"
+            rows={4}
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+          >
+            {" "}
+          </textarea>
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full
+           text-lg cursor-pointer"
+          >
+            Save
+          </button>
+        </form>
+        <img src={assets.logo_icon} alt="" className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
